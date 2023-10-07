129ebdc8c76b2c9b9f150be471bc5fe48641b20c
Yuji <g.yujinak@gmail.com>
2023-10-07 08:12:38 -0300

    adicionando botão claro/escuro

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const btnClaroEscuro = document.querySelector(".botao-claro-escuro");
+const pagina = document.body;
+
+btnClaroEscuro.addEventListener("click", function () {
+  pagina.classList.toggle("dark-mode");
+});
